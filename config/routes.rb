97e4154b97f08ebc1208f228fba28638fdba8d90
edwin sacalamitao <sacalamitao.edwin@yahoo.com.ph>
2021-11-06 01:13:12 +0800

    added:devise POST for sign_in and Sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :notes
 
+  # devise_scope :user do
+  #   post "sign_up", to: "registrations#create"
+  #   post "sign_in", to: "sessions#create"
+  # end
+
+  namespace :api, defaults: {format: :json} do
+    devise_scope :user do
+      post "sign_up", to: "registrations#create"
+      post "sign_in", to: "sessions#create"
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
